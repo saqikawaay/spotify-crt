4f4c6383f7573724e2bc254269159192706c53fe
Trevor Anderson <saqikawaay@gmail.com>
2023-12-25 23:22:02 +1000

    added login, backend dead, new CSS

---
+++ src/components/Login.jsx
@@ -0,0 +1,27 @@
+
+
+
+
+function Login() {
+    return (
+        <>
+        <div>
+        
+        <img src='src/assets/spotify_crt_2.png' className="logo" alt="" />
+    </div>
+    <h1 className='crt'>SPOTIFY CRT</h1>
+    <h3 className="typing">FOR THAT REAL LO-FI VIBE.</h3>
+    <div className="card" >
+      <button className="login-button" onClick={(console.log("you clicked"))}>
+        LOGIN
+      </button>
+      <p className="typing">
+        Log in to Spotify to start listening.
+      </p>
+    </div>
+    </>
+    );
+}
+
+
+export default Login;
